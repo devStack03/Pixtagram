b934bf269dcd0c7afbb2a12b8d6bc246eff572db
devStack03 <devking0325@gmail.com>
2022-08-26 09:52:07 +0900

    first commit: migrate from bitbucket to github

--- src/app/shared/config.ts
+++
@@ -1,9 +0,0 @@
-export enum AppConfig {
-     // siteUrl= 'http://localhost',
-     // apiURL = 'http://127.0.0.1:3000/api/',
-     siteUrl= 'https://newbiefans.com',
-     apiURL = 'https://newbiefans.com/api/',
-     Facebook_ID = '139559614492195',
-     Google_ID = '760763647036-prucscukjbvkq0og8fgatdgkm4k8akk1.apps.googleusercontent.com',
-     state = ''
-}
